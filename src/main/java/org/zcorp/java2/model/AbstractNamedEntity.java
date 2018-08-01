8d124aab770d19d3424ca189314af63a3d094886
weofferservice <weofferservice@hotmail.com>
2018-08-01 14:39:37 +0300

    Развитие слоя доменной модели приложения

---
+++ src/main/java/org/zcorp/java2/model/AbstractNamedEntity.java
@@ -0,0 +1,25 @@
+package org.zcorp.java2.model;
+
+public abstract class AbstractNamedEntity extends AbstractBaseEntity {
+
+    protected String name;
+
+    protected AbstractNamedEntity(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Entity %s (%s, '%s')", getClass().getName(), id, name);
+    }
+
+}
---
+++ src/main/java/org/zcorp/java2/model/Role.java
@@ -0,0 +1,6 @@
+package org.zcorp.java2.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
