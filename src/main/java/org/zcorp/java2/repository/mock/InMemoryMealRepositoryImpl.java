cd3d6974721ee3352e02b8b9868f1732b6a3fa3c
weofferservice <weofferservice@hotmail.com>
2018-08-02 23:55:33 +0300

    Добавление логирования в слои функциональности Meal

--- src/main/java/org/zcorp/java2/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/org/zcorp/java2/repository/mock/InMemoryMealRepositoryImpl.java
@@ -1,5 +1,6 @@
 package org.zcorp.java2.repository.mock;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Repository;
 import org.zcorp.java2.model.Meal;
 import org.zcorp.java2.repository.MealRepository;
@@ -12,9 +13,12 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.stream.Collectors.toList;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
+    private static final Logger log = getLogger(InMemoryMealRepositoryImpl.class);
+
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -24,6 +28,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
+        log.info("save {} for userId={}", meal, userId);
         if (meal.getUserId() != userId) {
             return null;
         }
@@ -37,6 +42,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public boolean delete(int id, int userId) {
+        log.info("delete {} for userId={}", id, userId);
         Meal meal = repository.get(id);
         if (meal == null || meal.getUserId() != userId) {
             return false;
@@ -47,6 +53,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal get(int id, int userId) {
+        log.info("get {} for userId={}", id, userId);
         Meal meal = repository.get(id);
         if (meal == null || meal.getUserId() != userId) {
             return null;
@@ -56,6 +63,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public List<Meal> getAll(int userId) {
+        log.info("getAll for userId={}", userId);
         return repository.values().stream()
                 .filter(meal -> meal.getUserId() == userId)
                 .sorted(Comparator.comparing(Meal::getDateTime).reversed())
--- src/main/java/org/zcorp/java2/web/meal/MealRestController.java
+++ src/main/java/org/zcorp/java2/web/meal/MealRestController.java
@@ -1,5 +1,6 @@
 package org.zcorp.java2.web.meal;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.zcorp.java2.model.Meal;
@@ -11,11 +12,16 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.zcorp.java2.util.ValidationUtil.assureIdConsistent;
+import static org.zcorp.java2.util.ValidationUtil.checkNew;
 import static org.zcorp.java2.web.SecurityUtil.authUserCaloriesPerDay;
 import static org.zcorp.java2.web.SecurityUtil.authUserId;
 
 @Controller
 public class MealRestController {
+    private static final Logger log = getLogger(MealRestController.class);
+
     private final MealService service;
 
     @Autowired
@@ -23,24 +29,30 @@ public class MealRestController {
         this.service = service;
     }
 
-    public void create(Meal meal) {
-        service.create(meal, authUserId());
+    public Meal create(Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        return service.create(meal, authUserId());
     }
 
     public void delete(int id) {
+        log.info("delete {}", id);
         service.delete(id, authUserId());
     }
 
     public Meal get(int id) {
+        log.info("get {}", id);
         return service.get(id, authUserId());
     }
 
     public void update(Meal meal, int id) {
-        meal.setId(id);
+        log.info("update {} with id={}", meal, id);
+        assureIdConsistent(meal, id);
         service.update(meal, authUserId());
     }
 
     public List<MealWithExceed> getFilteredWithExceeded(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+        log.info("getFilteredWithExceeded with startDate={}, endDate={}, startTime={}, endTime={}", startDate, endDate, startTime, endTime);
         return MealsUtil.getFilteredWithExceeded(service.getAll(authUserId()), authUserCaloriesPerDay(),
                 startDate, endDate,
                 startTime, endTime);
