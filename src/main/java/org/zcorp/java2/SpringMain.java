d017f6c4733085a5fbd4e803dea4ca470db7f58b
weofferservice <weofferservice@hotmail.com>
2018-08-02 23:57:49 +0300

    Решение 2.5 Включить классы еды в контекст Spring (добавить аннотации) и вызвать из SpringMain любой метод MealRestController (проверить что Spring все корректно заинжектил)

--- src/main/java/org/zcorp/java2/SpringMain.java
+++ src/main/java/org/zcorp/java2/SpringMain.java
@@ -2,11 +2,17 @@ package org.zcorp.java2;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.zcorp.java2.model.Meal;
 import org.zcorp.java2.model.Role;
 import org.zcorp.java2.model.User;
+import org.zcorp.java2.web.meal.MealRestController;
 import org.zcorp.java2.web.user.AdminRestController;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+
+import static org.zcorp.java2.util.UsersUtil.USER_ID;
 
 public class SpringMain {
     public static void main(String[] args) {
@@ -14,7 +20,15 @@ public class SpringMain {
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            User user = adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            System.out.println();
+            System.out.println(user);
+            System.out.println();
+            MealRestController mealRestController = appCtx.getBean(MealRestController.class);
+            Meal meal = mealRestController.create(new Meal(USER_ID, LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Еда", 1500));
+            System.out.println();
+            System.out.println(meal);
+            System.out.println();
         }
     }
 }
--- src/main/java/org/zcorp/java2/web/SecurityUtil.java
+++ src/main/java/org/zcorp/java2/web/SecurityUtil.java
@@ -1,11 +1,12 @@
 package org.zcorp.java2.web;
 
 import static org.zcorp.java2.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+import static org.zcorp.java2.util.UsersUtil.USER_ID;
 
 public class SecurityUtil {
 
     public static int authUserId() {
-        return 1;
+        return USER_ID;
     }
 
     public static int authUserCaloriesPerDay() {
