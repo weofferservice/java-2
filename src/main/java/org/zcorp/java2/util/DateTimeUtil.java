4ee3fc942749b825c6876ebdc98d0fc658f2416d
weofferservice <weofferservice@hotmail.com>
2018-08-03 21:03:49 +0300

    Рефакторинг DateTimeUtil

--- src/main/java/org/zcorp/java2/util/DateTimeUtil.java
+++ src/main/java/org/zcorp/java2/util/DateTimeUtil.java
@@ -6,8 +6,8 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static <T extends Comparable<? super T>> boolean isBetween(T local, T start, T end) {
-        return local.compareTo(start) >= 0 && local.compareTo(end) <= 0;
+    public static <T extends Comparable<? super T>> boolean isBetween(T value, T start, T end) {
+        return value.compareTo(start) >= 0 && value.compareTo(end) <= 0;
     }
 
     public static String toString(LocalDateTime ldt) {
