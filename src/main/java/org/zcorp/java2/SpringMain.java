b1fd9d3ab420894a544dd00ad8d4364880bf4d67
weofferservice <weofferservice@hotmail.com>
2018-08-04 00:38:43 +0300

    Проверка работоспособности фильтрации через метод getBetween в mealRestController

--- src/main/java/org/zcorp/java2/SpringMain.java
+++ src/main/java/org/zcorp/java2/SpringMain.java
@@ -5,14 +5,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.zcorp.java2.model.Meal;
 import org.zcorp.java2.model.Role;
 import org.zcorp.java2.model.User;
+import org.zcorp.java2.to.MealWithExceed;
 import org.zcorp.java2.web.SecurityUtil;
 import org.zcorp.java2.web.meal.MealRestController;
 import org.zcorp.java2.web.user.AdminRestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.List;
 
+import static org.zcorp.java2.util.UsersUtil.ADMIN_ID;
 import static org.zcorp.java2.util.UsersUtil.USER_ID;
 
 public class SpringMain {
@@ -27,14 +33,21 @@ public class SpringMain {
             System.out.println(user);
             System.out.println();
 
-            SecurityUtil.setAuthUserId(user.getId());
+            SecurityUtil.setAuthUserId(USER_ID);
             MealRestController mealRestController = appCtx.getBean(MealRestController.class);
+            List<MealWithExceed> filteredMealsWithExceeded = mealRestController.getBetween(
+                    LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                    LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+            System.out.println();
+            filteredMealsWithExceeded.forEach(System.out::println);
+            System.out.println();
+
             Meal meal = mealRestController.create(new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Еда", 1500));
             System.out.println();
             System.out.println(meal);
             System.out.println();
 
-            SecurityUtil.setAuthUserId(USER_ID);
+            SecurityUtil.setAuthUserId(ADMIN_ID);
             Meal newMeal = new Meal(meal.getId(), LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Еда", 1000);
             mealRestController.update(newMeal, newMeal.getId());
         }
