f537560dc128aee8f9f3a5a71ec8ae7b3f253c8b
weofferservice <weofferservice@hotmail.com>
2018-08-03 13:53:46 +0300

    Проверка сценария: авторизованный пользователь пробует изменить чужую еду (id еды ему не принадлежит)

--- src/main/java/org/zcorp/java2/SpringMain.java
+++ src/main/java/org/zcorp/java2/SpringMain.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.zcorp.java2.model.Meal;
 import org.zcorp.java2.model.Role;
 import org.zcorp.java2.model.User;
+import org.zcorp.java2.web.SecurityUtil;
 import org.zcorp.java2.web.meal.MealRestController;
 import org.zcorp.java2.web.user.AdminRestController;
 
@@ -19,16 +20,23 @@ public class SpringMain {
         // java 7 Automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             User user = adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
             System.out.println();
             System.out.println(user);
             System.out.println();
+
+            SecurityUtil.setAuthUserId(user.getId());
             MealRestController mealRestController = appCtx.getBean(MealRestController.class);
-            Meal meal = mealRestController.create(new Meal(USER_ID, LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Еда", 1500));
+            Meal meal = mealRestController.create(new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Еда", 1500));
             System.out.println();
             System.out.println(meal);
             System.out.println();
+
+            SecurityUtil.setAuthUserId(USER_ID);
+            Meal newMeal = new Meal(meal.getId(), LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Еда", 1000);
+            mealRestController.update(newMeal, newMeal.getId());
         }
     }
 }
