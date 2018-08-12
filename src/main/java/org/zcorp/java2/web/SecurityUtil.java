2e21edef5d250fa1d31a176bb2ec7dc8edc048a0
weofferservice <weofferservice@hotmail.com>
2018-08-12 10:41:23 +0300

    1) Перемещение класса UserTestData на уровень тестов 2) Подключение в проект библиотеки AssertJ (для тестов)

--- src/main/java/org/zcorp/java2/web/SecurityUtil.java
+++ src/main/java/org/zcorp/java2/web/SecurityUtil.java
@@ -1,11 +1,11 @@
 package org.zcorp.java2.web;
 
 import static org.zcorp.java2.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
-import static org.zcorp.java2.UserTestData.USER_ID;
+import static org.zcorp.java2.model.AbstractBaseEntity.START_SEQ;
 
 public class SecurityUtil {
 
-    private static int authUserId = USER_ID;
+    private static int authUserId = START_SEQ;
 
     public static int authUserId() {
         return authUserId;
