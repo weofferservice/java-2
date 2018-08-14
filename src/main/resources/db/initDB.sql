f3947494acae6005317743abd41d4dd5fab4aca7
weofferservice <weofferservice@hotmail.com>
2018-08-14 22:26:36 +0300

    Решение 3.2 Дополнить скрипты создания и инициализации БД таблицей meals. Запустить скрипты на БД (через Run). Порядок таблиц при DROP и DELETE важен, если они связаны внешними ключами (Foreign Key, FK)

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -22,4 +23,13 @@ CREATE TABLE user_roles
   role    VARCHAR NOT NULL,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  date_time   TIMESTAMP DEFAULT now() NOT NULL,
+  description TEXT                    NOT NULL,
+  calories    INTEGER DEFAULT 1000    NOT NULL,
+  user_id     INTEGER                 NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
