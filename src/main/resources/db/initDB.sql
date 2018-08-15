523421bc9a4bdd22782888835ae86b6385cd4d38
weofferservice <weofferservice@hotmail.com>
2018-08-15 13:40:11 +0300

    Решение 3.6 Сделать индексы к таблице meals: запретить создавать у одного и того же юзера еду с одинаковой dateTime (помните, что индекс на pk (id) PostgreSQL создает автоматически)

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -33,3 +33,4 @@ CREATE TABLE meals
   calories    INTEGER DEFAULT 1000    NOT NULL,
   user_id     INTEGER                 NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
+CREATE UNIQUE INDEX meals_unique_user_id_date_time_idx ON meals (user_id, date_time);
