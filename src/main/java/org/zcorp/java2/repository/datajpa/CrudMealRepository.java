37674e03d62942624902287751096a3ddfc15bf4
weofferservice <weofferservice@hotmail.com>
2018-08-28 20:01:19 +0300

    Решение 5.1 Имплементировать DataJpaMealRepositoryImpl

--- src/main/java/org/zcorp/java2/repository/datajpa/CrudMealRepository.java
+++ src/main/java/org/zcorp/java2/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,38 @@
 package org.zcorp.java2.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import org.zcorp.java2.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Override
+    Optional<Meal> findById(Integer id);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId")
+    List<Meal> getAll(@Param("userId") int userId, Sort sort);
+
+    @SuppressWarnings("JpaQlInspection")
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDateTime AND :endDateTime")
+    List<Meal> getBetween(@Param("startDateTime") LocalDateTime startDateTime,
+                          @Param("endDateTime") LocalDateTime endDateTime,
+                          @Param("userId") int userId,
+                          Sort sort);
 }
--- src/main/java/org/zcorp/java2/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/org/zcorp/java2/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -1,42 +1,62 @@
 package org.zcorp.java2.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.zcorp.java2.model.Meal;
+import org.zcorp.java2.model.User;
 import org.zcorp.java2.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class DataJpaMealRepositoryImpl implements MealRepository {
 
+    private static final Sort SORT_DATETIME = new Sort(Sort.Direction.DESC, "dateTime");
+
     @Autowired
     private CrudMealRepository crudRepository;
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        User user = em.getReference(User.class, userId);
+        meal.setUser(user);
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal meal = crudRepository.findById(id).orElse(null);
+        if (meal == null || meal.getUser().getId() != userId) {
+            return null;
+        }
+        return meal;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAll(userId, SORT_DATETIME);
     }
 
     @Override
-    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+    public List<Meal> getBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return crudRepository.getBetween(startDateTime, endDateTime, userId, SORT_DATETIME);
     }
 
 }
