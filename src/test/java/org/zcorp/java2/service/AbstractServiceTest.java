827a9e4074b77277d751292d9cd285e378b25026
weofferservice <weofferservice@hotmail.com>
2018-09-18 17:31:21 +0300

    Вынос правил тестов Stopwatch и печати таблицы результатов в отдельный класс TimingRules

--- src/test/java/org/zcorp/java2/service/AbstractServiceTest.java
+++ src/test/java/org/zcorp/java2/service/AbstractServiceTest.java
@@ -1,12 +1,11 @@
 package org.zcorp.java2.service;
 
-import org.junit.AfterClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,10 +13,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zcorp.java2.ActiveDbProfileResolver;
-
-import java.util.concurrent.TimeUnit;
-
-import static org.slf4j.LoggerFactory.getLogger;
+import org.zcorp.java2.TimingRules;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -34,30 +30,11 @@ public abstract class AbstractServiceTest {
         SLF4JBridgeHandler.install();
     }
 
-    private static final Logger log = getLogger("result");
-
-    private static StringBuilder results = new StringBuilder();
-
-    @AfterClass
-    public static void printResult() {
-        log.info("\n-------------------------------------------------------------------------------------------------------" +
-                "\nTest                                                                                       Duration, ms" +
-                "\n-------------------------------------------------------------------------------------------------------\n" +
-                results +
-                "-------------------------------------------------------------------------------------------------------\n");
-        results.setLength(0);
-    }
+    @ClassRule
+    public static ExternalResource summary = TimingRules.SUMMARY;
 
     @Rule
-    //https://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-business-releva
-    public Stopwatch stopwatch = new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
-            results.append(result).append('\n');
-            log.info(result + " ms\n");
-        }
-    };
+    public Stopwatch stopwatch = TimingRules.STOPWATCH;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
