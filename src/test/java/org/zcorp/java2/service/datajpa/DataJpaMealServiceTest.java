502aba457ce9b6c21ee429675460379d0f2dd87b
weofferservice <weofferservice@hotmail.com>
2018-09-19 14:22:22 +0300

    Переделал тесты на методы получения юзера с едой и еды с юзером через две отдельных проверки (отдельно проверяем еду, отдельно юзера)

--- src/test/java/org/zcorp/java2/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/org/zcorp/java2/service/datajpa/DataJpaMealServiceTest.java
@@ -2,13 +2,15 @@ package org.zcorp.java2.service.datajpa;
 
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import org.zcorp.java2.UserTestData;
+import org.zcorp.java2.model.Meal;
 import org.zcorp.java2.service.AbstractMealServiceTest;
 import org.zcorp.java2.util.exception.NotFoundException;
 
 import static org.zcorp.java2.MealTestData.*;
 import static org.zcorp.java2.Profiles.DATAJPA;
+import static org.zcorp.java2.UserTestData.ADMIN;
 import static org.zcorp.java2.UserTestData.ADMIN_ID;
-import static org.zcorp.java2.UserTestData.USER_ID;
 
 @ActiveProfiles(DATAJPA)
 public class DataJpaMealServiceTest extends AbstractMealServiceTest {
@@ -16,7 +18,10 @@ public class DataJpaMealServiceTest extends AbstractMealServiceTest {
     @Test
     @Override
     public void getWithUser() {
-        assertMatchWithUser(service.getWithUser(MEAL1_ID, USER_ID), MEAL1);
+//        assertMatchWithUser(service.getWithUser(MEAL1_ID, USER_ID), MEAL1);
+        Meal adminMeal = service.getWithUser(ADMIN_MEAL1_ID, ADMIN_ID);
+        assertMatch(adminMeal, ADMIN_MEAL1);
+        UserTestData.assertMatch(adminMeal.getUser(), ADMIN);
     }
 
     @Test
--- src/test/java/org/zcorp/java2/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/org/zcorp/java2/service/datajpa/DataJpaUserServiceTest.java
@@ -2,10 +2,12 @@ package org.zcorp.java2.service.datajpa;
 
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import org.zcorp.java2.MealTestData;
 import org.zcorp.java2.model.User;
 import org.zcorp.java2.service.AbstractUserServiceTest;
 import org.zcorp.java2.util.exception.NotFoundException;
 
+import static org.zcorp.java2.MealTestData.MEALS;
 import static org.zcorp.java2.Profiles.DATAJPA;
 import static org.zcorp.java2.UserTestData.*;
 
@@ -16,7 +18,9 @@ public class DataJpaUserServiceTest extends AbstractUserServiceTest {
     @Override
     public void getWithMeals() {
         User user = service.getWithMeals(USER_ID);
-        assertMatchWithMeals(user, USER);
+//        assertMatchWithMeals(user, USER);
+        assertMatch(user, USER);
+        MealTestData.assertMatch(user.getMeals(), MEALS);
     }
 
     @Test(expected = NotFoundException.class)
