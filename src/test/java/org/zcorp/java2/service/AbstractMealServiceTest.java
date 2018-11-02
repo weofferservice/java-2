e7cfa3e6d55fa46e5dbeef193c35aa1ba85c4c43
weofferservice <weofferservice@hotmail.com>
2018-11-02 17:19:21 +0300

    Рефакторинг теста testValidation() с заменой сообщения об ошибке на более понятное

--- src/test/java/org/zcorp/java2/service/AbstractMealServiceTest.java
+++ src/test/java/org/zcorp/java2/service/AbstractMealServiceTest.java
@@ -101,7 +101,7 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     public void testValidation() {
-        Assumptions.assumeTrue(isJpaBased(), "JPA not supported");
+        Assumptions.assumeTrue(isJpaBased(), "Validation not supported (only for JPA)");
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "           ",  300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null,                                             null, "Description",  300), USER_ID), NullPointerException.class);
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description",    9), USER_ID), ConstraintViolationException.class);
