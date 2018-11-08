4bd49b8fb27dd2fae7145b851d3b62332aa05a25
weofferservice <weofferservice@hotmail.com>
2018-11-08 21:30:15 +0300

    Рефакторинг: добавляем атрибут produces = MediaType.APPLICATION_JSON_VALUE в аннотацию к методу фильтрации getBetween

--- src/main/java/org/zcorp/java2/web/meal/MealAjaxController.java
+++ src/main/java/org/zcorp/java2/web/meal/MealAjaxController.java
@@ -45,7 +45,7 @@ public class MealAjaxController extends AbstractMealController {
     }
 
     @Override
-    @GetMapping("/filter")
+    @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<MealWithExceed> getBetween(@RequestParam(required = false) LocalDate startDate,
                                            @RequestParam(required = false) LocalTime startTime,
                                            @RequestParam(required = false) LocalDate endDate,
