e45df0ff202e417d995a9d24a678426d6caf57a0
weofferservice <weofferservice@hotmail.com>
2018-11-11 15:36:38 +0300

    Из-за кардинальных изменений в классе SecurityUtil поломались тесты. Для решения этой проблемы: 1) добавляем в проект библиотеку Spring Security Test 2) в класс TestUtil добавляем методы эмуляции базовой userHttpBasic и обычной userAuth авторизаций

--- src/test/java/org/zcorp/java2/TestUtil.java
+++ src/test/java/org/zcorp/java2/TestUtil.java
@@ -3,8 +3,10 @@ package org.zcorp.java2;
 import org.slf4j.Logger;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.zcorp.java2.model.User;
 import org.zcorp.java2.web.json.JsonUtil;
 
@@ -47,6 +49,14 @@ public class TestUtil {
     public static void mockAuthorize(User user) {
         SecurityContextHolder.getContext().setAuthentication(
                 new UsernamePasswordAuthenticationToken(new AuthorizedUser(user), null, user.getRoles()));
+    }
+
+    public static RequestPostProcessor userHttpBasic(User user) {
+        return SecurityMockMvcRequestPostProcessors.httpBasic(user.getEmail(), user.getPassword());
+    }
+
+    public static RequestPostProcessor userAuth(User user) {
+        return SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
     }
 
 }
