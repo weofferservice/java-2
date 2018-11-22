a5a56ae936efe6eb6f58240f9b5872c1cea572d9
weofferservice <weofferservice@hotmail.com>
2018-11-22 13:45:03 +0300

    Создаем отдельную валидационную группу для проверки части данных только на входе, чтобы не проверять их при записи в БД

--- src/main/java/org/zcorp/java2/ValidationGroup.java
+++ src/main/java/org/zcorp/java2/ValidationGroup.java
@@ -3,5 +3,9 @@ package org.zcorp.java2;
 import javax.validation.groups.Default;
 
 public class ValidationGroup {
+    // Validate only for form/AJAX/REST
+    public interface Web extends Default {}
+
+    // Validate only when DB save/update
     public interface Persist extends Default {}
 }
